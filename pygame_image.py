2a7b7dbe527841fbda15bb338a0f15ef914c0b7e
c0a241160b <c0a241160b@edu.teu.ac.jp>
2025-06-10 15:53:15 +0900

    練習9：背景画像のループ

--- pygame_image.py
+++ pygame_image.py
@@ -21,9 +21,12 @@ def main():
 
         screen.blit(bg_img, [tmr, 0])
         screen.blit(bg_img2, [tmr+1600, 0])
+        screen.blit(bg_img, [tmr+3200, 0])
         screen.blit(kk_img, [300,200])
         pg.display.update()
-        tmr -= 1        
+        tmr += -1
+        if tmr==-3199:
+            tmr=0        
         clock.tick(200)
 
 
